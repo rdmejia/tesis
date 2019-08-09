d8cea5ec1109955d381f9d659e3dab2638fe6678
Daniel Mejia <rdmejiaa@gmail.com>
2019-08-08 20:11:57 -0600

    Fix retry-connection mechanism

--- src/gui.py
+++ src/gui.py
@@ -238,9 +238,9 @@ class GUI:
                 i = i + 1
                 print "Connection to %s failed. Retrying..." % mw_mac
             
-            if not connected:
-                print "Error connecting to %s" % mw_mac
-                return
+        if not connected:
+            print "Error connecting to %s" % mw_mac
+            return
 
         pattern = LedPattern(repeat_count= Const.LED_REPEAT_INDEFINITELY)
         libmetawear.mbl_mw_led_load_preset_pattern(byref(pattern), LedPreset.SOLID)
